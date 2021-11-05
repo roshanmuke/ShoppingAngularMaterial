37a980e84b1e382a465279332da307ab63148256
roshanmuke <66641740+roshanmuke@users.noreply.github.com>
2021-11-05 08:13:53 +0530

    Forgot Password Functionality

--- src/app/auth/components/forgotpass/forgotpass.component.ts
+++ src/app/auth/components/forgotpass/forgotpass.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup , FormControl , Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-forgotpass',
@@ -7,9 +8,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ForgotpassComponent implements OnInit {
 
+forgotForm;
+email;
+
   constructor() { }
 
   ngOnInit(): void {
+
+    this.forgotForm = new FormGroup({
+      email: new FormControl('', [Validators.required, Validators.minLength(6)])
+    });
   }
 
 }
